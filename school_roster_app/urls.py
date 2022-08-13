088b7fe212bfafd4d4c181e24baacb45ace1cccd
Rexford Wiafe <w.rexford@yahoo.com>
2022-08-13 08:51:36 -0500

    final changes

--- school_roster_app/urls.py
+++ school_roster_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('staff/', views.list_staff, name='list_staff'),
     path("staff/<int:employee_id>/", views.staff_detail, name="staff_detail"),
     path('students/', views.list_students, name='list_students'),
